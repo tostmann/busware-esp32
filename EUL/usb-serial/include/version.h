2aa75f6fcaa5108c0766ce2d01dd65557f5e6529
Dirk Tostmann <tostmann@busware.de>
2024-01-02 23:34:39 +0100

    changed funding link

--- EUL/usb-serial/include/version.h
+++ EUL/usb-serial/include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "10"
+  #define BUILD_NUMBER "12"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.1.10 - 2024-01-02 19:01:17.448801"
+  #define VERSION "v1.1.12 - 2024-01-02 23:16:30.366175"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.1.10"
+  #define VERSION_SHORT "v1.1.12"
 #endif
