adf2f95570fd8fdf5ad525f2f70b987acc0af4a8
Dirk Tostmann <tostmann@busware.de>
2024-01-02 23:43:05 +0100

    added defines for busware internals

--- EUL/usb-serial/include/version.h
+++ EUL/usb-serial/include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "12"
+  #define BUILD_NUMBER "13"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.1.12 - 2024-01-02 23:16:30.366175"
+  #define VERSION "v1.1.13 - 2024-01-02 23:41:23.077929"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.1.12"
+  #define VERSION_SHORT "v1.1.13"
 #endif
