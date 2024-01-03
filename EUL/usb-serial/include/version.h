4141b152ee48c9a5a50817751b1fa0dbc6cdee26
Dirk Tostmann <tostmann@busware.de>
2024-01-03 11:30:53 +0100

    fixed CORS trouble

--- EUL/usb-serial/include/version.h
+++ EUL/usb-serial/include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "13"
+  #define BUILD_NUMBER "16"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.1.13 - 2024-01-02 23:41:23.077929"
+  #define VERSION "v1.1.16 - 2024-01-02 23:48:00.806180"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.1.13"
+  #define VERSION_SHORT "v1.1.16"
 #endif
